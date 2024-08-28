48fac4a20164bab6eee25b6182d3f5507dfcda6b
ALADETAN-IFE <163364168+ALADETAN-IFE@users.noreply.github.com>
2024-08-28 12:00:50 +0100

    did routing system

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import './App.css'
-import { RouterProvider } from 'react-router-dom'
-import Routes from './routes/routes'
-import { ToastContainer } from 'react-toastify'
+import React from 'react''
+import './App.css''
+import { RouterProvider } from 'react-router-dom';
+import Routes from './routes/routes';
+import { ToastContainer } from 'react-toastify';
 
 const App = () => {
   return (
