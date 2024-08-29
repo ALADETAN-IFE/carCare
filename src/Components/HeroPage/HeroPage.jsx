425127fa4389249073d07a2a84f8dc17f0295a5f
ALADETAN-IFE <famousfotune123@gmail.com>
2024-08-29 14:09:09 +0100

    started the homepage

--- src/Components/HeroPage/HeroPage.jsx
+++ src/Components/HeroPage/HeroPage.jsx
@@ -3,6 +3,7 @@ import './heroPage.css'
 const HeroPage = () => {
   return (
     <div className='HeroPage'>
+        l
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam porro magnam similique illum sunt culpa, ea libero quae blanditiis voluptatum voluptate expedita voluptatem ab quidem explicabo natus alias quos tempore quaerat, laboriosam, tempora modi architecto! Soluta iure minima cum! Cupiditate!
     </div>
   )
