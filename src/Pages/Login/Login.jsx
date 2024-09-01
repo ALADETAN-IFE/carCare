76e5cdf5ab8885d1404a8f57654027e73a167e53
CoveredTechSis <aishaolalekan@gmail.com>
2024-09-01 23:58:17 +0200

    the login page

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
 import './Login.css'
+import login from '../../assets/images/login.png'
 
 const Login = () => {
   return (
-    <div>Login</div>
+   <div className='Login__container'>
+       <div>
+        <img src={login} alt="" />
+       </div>
+       <div className='Login__details'>
+        <h1>WELCOME BACK</h1>
+        <p>Access your dashboard to book appointments, track your <br />
+        service history, and manage your car care with ease.</p>
+        <form action="">
+         <div>
+         <label htmlFor="">Email Address</label>
+         <input type="text" placeholder='Enter your email address'/>
+         </div>
+         <div>
+         <label htmlFor="">Password</label>
+         <input type="text" placeholder='Enter your password'/>
+         <p><Link to='/forgotPassword'>Forgot Password ?</Link></p>
+         </div>
+        </form>
+        <button type="submit">Login</button>
+        <div className="underbutton__text">
+        <p>New in Carcare? <Link to='/signup' className='signup__link'>SignUp</Link></p>
+        </div>
+       </div>
+   </div>
   )
 }
+
+
 
 export default Login
