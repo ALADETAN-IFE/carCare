8dcef94d59c4584dcd2bd554d06db867a97c0cb7
ALADETAN-IFE <famousfotune123@gmail.com>
2024-09-02 00:04:40 +0100

    created the codefiles for the inside of the app

---
+++ src/Pages/App/Mechanic/Mechanics.jsx
@@ -0,0 +1,9 @@
+import './mechanic.css'
+
+const Mechanic = () => {
+  return (
+    <div>Mechanic</div>
+  )
+}
+
+export default Mechanic
---
+++ src/Pages/ForgotPassword/ForgotPassword.jsx
@@ -0,0 +1,9 @@
+import './forgotPassword.css'
+
+const ForgotPassword = () => {
+  return (
+    <div className='ForgotPassword'>ForgotPassword</div>
+  )
+}
+
+export default ForgotPassword
