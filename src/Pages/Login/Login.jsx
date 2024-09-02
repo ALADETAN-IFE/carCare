982d989411b60cafcdeaf02ab72d285bd2944993
CoveredTechSis <aishaolalekan@gmail.com>
2024-09-02 04:16:33 +0200

    update on the login page

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
-import './Login.css'
-import login from '../../assets/images/login.png'
+import {React, useState} from 'react';
+import './Login.css';
+import login from '../../assets/images/login.png';
+import { Link } from 'react-router-dom';
+import { IoEye, IoEyeOff } from 'react-icons/io5';
 
 const Login = () => {
+  const [seePassword, setSeePassword] = useState(true)
   return (
    <div className='Login__container'>
        <div>
@@ -12,20 +15,23 @@ const Login = () => {
         <h1>WELCOME BACK</h1>
         <p>Access your dashboard to book appointments, track your <br />
         service history, and manage your car care with ease.</p>
-        <form action="">
-         <div>
+        <form action="" className='login__form'>
+         <div className='login__input'>
          <label htmlFor="">Email Address</label>
          <input type="text" placeholder='Enter your email address'/>
          </div>
-         <div>
+         <div className='login__input'>
          <label htmlFor="">Password</label>
-         <input type="text" placeholder='Enter your password'/>
-         <p><Link to='/forgotPassword'>Forgot Password ?</Link></p>
+         <div className='login__input__password'>
+         <input type="password" placeholder='Enter your password'/>
+         {seePassword ? <IoEyeOff onClick={() => setSeePassword(!seePassword )} /> : <IoEye onClick={() => setSeePassword(!seePassword)} />}
+         </div>
+         <p className='login__forgot__password'><Link to='/forgotPassword'>Forgot Password ?</Link></p>
          </div>
         </form>
-        <button type="submit">Login</button>
+        <button type="submit" className='login__btn'>Login</button>
         <div className="underbutton__text">
-        <p>New in Carcare? <Link to='/signup' className='signup__link'>SignUp</Link></p>
+        <p>New in Carcare?  <Link to='/signup' className='signup__link'>SignUp</Link></p>
         </div>
        </div>
    </div>
