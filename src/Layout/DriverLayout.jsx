589f41fa3a71485bcb3a17a291022dacd87a9a11
ALADETAN-IFE <famousfotune123@gmail.com>
2024-09-07 16:14:14 +0100

    started dashboard and i have finished home page layouts

--- src/Layout/DriverLayout.jsx
+++ src/Layout/DriverLayout.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import "./layout.css"
 import { Outlet } from 'react-router-dom'
+import LayoutHeader from "./LayoutHeader/LayoutHeader"
+import SideBar from "./SideBar/SideBar"
 
 const DriverLayout = () => {
   return (
-    <div>
-        <Outlet/>
+    <div className="layout">
+      <LayoutHeader />
+      <div className="layoutDown">
+      <SideBar/>
+        <Outlet />
+      </div>
     </div>
   )
 }
---
+++ src/Layout/SideBar/SideBar.jsx
@@ -0,0 +1,9 @@
+import "./sideBar.css"
+
+const SideBar = () => {
+  return (
+    <div className="sidebar">SideBar</div>
+  )
+}
+
+export default SideBar
