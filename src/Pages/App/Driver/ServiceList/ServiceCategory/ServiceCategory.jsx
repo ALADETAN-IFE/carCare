be9378a0de99347253d38e09ae9b35d81f2716d9
CoveredTechSis <aishaolalekan@gmail.com>
2024-09-10 19:06:47 +0200

    updated changes

--- src/Pages/App/Driver/ServiceList/ServiceCategory/ServiceCategory.jsx
+++ src/Pages/App/Driver/ServiceList/ServiceCategory/ServiceCategory.jsx
@@ -1,10 +1,51 @@
 import React from 'react'
 import './ServiceCategory.css'
+import { NavLink, useNavigate } from 'react-router-dom'
 
-const ServiceCategory = () => {
+const ServiceCategory = (seePages, setSeePages) => {
+    const navigate = useNavigate()
+    const categoryOption = [
+        {
+            text: 'Tire & Wheels',
+            to: "tire"
+        },
+        {
+            text: "General Service",
+            to: "service",
+        },
+        {
+            text: "AC Service and Repairs",
+            to: "ACrepair",
+        },
+        {
+            text: "Brake System",
+            to: "brake",
+        },
+        {
+            text: "Preventive Service",
+            to: "prevent",
+        },
+        {
+            text: "Windsheild & Glass",
+            to: "glass",
+        },
+        {
+            text: "Steering System",
+            to: "steering"
+        },
+
+    ]
   return (
     <div className='serviceCategory__container'>
-        <div className=''>
+        <div className='categoryHeader'>
+           <h1>Service Category</h1>
+           <p>Select a category</p>
+        </div>
+        <div className='categoryOption'>
+            {categoryOption ?.map((e, i) => (
+                <NavLink className={pages.includes(e?.to)  ? "categoryOptionActive categoryOption" : "categoryOption"} key={i} onClick={()=> setSeePages(e?.to)}
+                >{e?.text}</NavLink>
+            ))}
 
         </div>
     </div>
