55b81df05a524d58df4e19bdd1dbfd6cde904d62
CoveredTechSis <aishaolalekan@gmail.com>
2024-09-20 14:12:54 +0200

    latest commit on about page

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -2,6 +2,7 @@ import Footer from "../../Components/Footer/Footer"
 import Header from "../../Components/Header/Header"
 import "./about.css"
 import AboutHeroPage from "./AboutDetails/AboutHeroPage"
+import OurMission from "./AboutDetails/OurMission/OurMission"
 import OurStory from "./AboutDetails/OurStory/OurStory"
 // import AboutHeroPage from "./AboutDetails/AboutDetails"
 
@@ -11,6 +12,7 @@ const About = () => {
         <Header/>
         <AboutHeroPage/>
         <OurStory/>
+        <OurMission/>
         <Footer/>
     </div>
   )
---
+++ src/Pages/About/AboutDetails/OurMission/OurMission.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./OurMission.css"
+import growYourbusiness from '../../../../assets/images/growYourbusiness.png'
+
+const OurMission = () => {
+  return (
+    <div className='OurMission'>
+        <div className='OurMissionLeft'>
+            <h1>Our Mission</h1>
+            <p>Our mission is to revolutionize the car maintenance industry by providing a reliable, convenient platform that puts you in control of your vehicle’s care. We strive to build a community where trust, transparency, and quality service are at the forefront.</p>
+
+        </div>
+        <div className='OurMissionRight'>
+          <img src={growYourbusiness} alt="" />
+        </div>
+
+    </div>
+  )
+}
+
+export default OurMission
