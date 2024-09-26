adc16f08f24de4968da24cbae7d478a0d8d6bc70
ALADETAN-IFE <famousfotune123@gmail.com>
2024-09-26 14:46:56 +0100

    fixed mech login

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -71,9 +71,9 @@ const Login = () => {
             setTimeout(() => {
               navigate("/mechInfo")
             }, 2000);
-            dispatch(logIn())
             dispatch(setTypeOfUser("Mechanic"))
           } else {
+            dispatch(logIn())
             // Success alert with navigation after 3 seconds
             Swal.fire({
               icon: 'success',
